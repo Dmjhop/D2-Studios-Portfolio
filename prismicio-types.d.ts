2849d92b0ac89dcb0e7a77f0b9dbe00b6578363f
David Hopkins II <dhscience2@gmail.com>
2024-08-16 21:04:34 -0400

    should be good

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -168,6 +168,7 @@ export type HomepageDocument<Lang extends string = string> =
   >;
 
 type PageDocumentDataSlicesSlice =
+  | ExperienceSlice
   | ContentIndexSlice
   | TechListSlice
   | BiographySlice;
@@ -661,6 +662,108 @@ export type ContentIndexSlice = prismic.SharedSlice<
 >;
 
 /**
+ * Item in *Experience → Default → Primary → Exp Item*
+ */
+export interface ExperienceSliceDefaultPrimaryExpItemItem {
+  /**
+   * Title field in *Experience → Default → Primary → Exp Item*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: experience.default.primary.exp_item[].title
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  title: prismic.KeyTextField;
+
+  /**
+   * Time Period field in *Experience → Default → Primary → Exp Item*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: experience.default.primary.exp_item[].time_period
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  time_period: prismic.KeyTextField;
+
+  /**
+   * Institution field in *Experience → Default → Primary → Exp Item*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: experience.default.primary.exp_item[].institution
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  institution: prismic.KeyTextField;
+
+  /**
+   * Description field in *Experience → Default → Primary → Exp Item*
+   *
+   * - **Field Type**: Rich Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: experience.default.primary.exp_item[].description
+   * - **Documentation**: https://prismic.io/docs/field#rich-text-title
+   */
+  description: prismic.RichTextField;
+}
+
+/**
+ * Primary content in *Experience → Default → Primary*
+ */
+export interface ExperienceSliceDefaultPrimary {
+  /**
+   * Heading field in *Experience → Default → Primary*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: experience.default.primary.heading
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  heading: prismic.KeyTextField;
+
+  /**
+   * Exp Item field in *Experience → Default → Primary*
+   *
+   * - **Field Type**: Group
+   * - **Placeholder**: *None*
+   * - **API ID Path**: experience.default.primary.exp_item[]
+   * - **Documentation**: https://prismic.io/docs/field#group
+   */
+  exp_item: prismic.GroupField<
+    Simplify<ExperienceSliceDefaultPrimaryExpItemItem>
+  >;
+}
+
+/**
+ * Default variation for Experience Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type ExperienceSliceDefault = prismic.SharedSliceVariation<
+  "default",
+  Simplify<ExperienceSliceDefaultPrimary>,
+  never
+>;
+
+/**
+ * Slice variation for *Experience*
+ */
+type ExperienceSliceVariation = ExperienceSliceDefault;
+
+/**
+ * Experience Shared Slice
+ *
+ * - **API ID**: `experience`
+ * - **Description**: Experience
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type ExperienceSlice = prismic.SharedSlice<
+  "experience",
+  ExperienceSliceVariation
+>;
+
+/**
  * Primary content in *Hero → Default → Primary*
  */
 export interface HeroSliceDefaultPrimary {
@@ -928,6 +1031,11 @@ declare module "@prismicio/client" {
       ContentIndexSliceDefaultPrimary,
       ContentIndexSliceVariation,
       ContentIndexSliceDefault,
+      ExperienceSlice,
+      ExperienceSliceDefaultPrimaryExpItemItem,
+      ExperienceSliceDefaultPrimary,
+      ExperienceSliceVariation,
+      ExperienceSliceDefault,
       HeroSlice,
       HeroSliceDefaultPrimary,
       HeroSliceVariation,
--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 
-import React, { useEffect, useRef, useState } from "react"
-import { asImageSrc, Content, isFilled } from "@prismicio/client"
-import { MdArrowDownward } from "react-icons/md"
-import Link from "next/link"
-import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
+import React, { useEffect, useRef, useState } from "react";
+import { asImageSrc, Content, isFilled } from "@prismicio/client";
+import { MdArrowDownward } from "react-icons/md";
+import Link from "next/link";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 type ContentListProps = {
-  items: Content.BlogPostDocument[] | Content.ProjectDocument[]
-  contentType: Content.ContentIndexSlice["primary"]["content_type"]
-  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"]
-  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"]
-}
+  items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
 
 function ContentList({
   items,
@@ -22,15 +22,15 @@ function ContentList({
   fallbackItemImage,
   viewMoreText = "Read More",
 }: ContentListProps) {
-  const component = useRef(null)
-  const itemsRef = useRef<Array<HTMLLIElement | null>>([])
-  const revealRef = useRef(null)
+  const component = useRef(null);
+  const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
+  const revealRef = useRef(null);
 
-  const [currentItem, setCurrentItem] = useState<null | number>(null)
+  const [currentItem, setCurrentItem] = useState<null | number>(null);
 
-  const lastMousePos = useRef({ x: 0, y: 0 })
+  const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blog" ? "/blog" : "/project"
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/projects";
 
   useEffect(() => {
     let ctx = gsap.context(() => {
@@ -48,23 +48,23 @@ function ContentList({
               end: "bottom center",
               toggleActions: "play none none none",
             },
-          }
-        )
-      })
-      return () => ctx.revert()
-    }, component)
-  }, [])
+          },
+        );
+      });
+      return () => ctx.revert();
+    }, component);
+  }, []);
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      const mousePos = { x: e.clientX, y: e.clientY + window.scrollY }
+      const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
 
-      const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2))
+      const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
 
       let ctx = gsap.context(() => {
         if (currentItem !== null) {
-          const maxY = window.scrollY + window.innerHeight - 350
-          const maxX = window.innerWidth - 150
+          const maxY = window.scrollY + window.innerHeight - 350;
+          const maxX = window.innerWidth - 150;
 
           gsap.to(revealRef.current, {
             x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
@@ -73,74 +73,77 @@ function ContentList({
             ease: "back.out(2)",
             duration: 1.3,
             opacity: 1,
-          })
+          });
         }
-        lastMousePos.current = mousePos
+        lastMousePos.current = mousePos;
 
-        return () => ctx.revert()
-      }, component)
-    }
+        return () => ctx.revert();
+      }, component);
+    };
 
-    window.addEventListener("mousemove", handleMouseMove)
+    window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      window.removeEventListener("mousemove", handleMouseMove)
-    }
-  }, [currentItem])
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [currentItem]);
 
   const contentImages = items.map((item) => {
     const image = isFilled.image(item.data.hover_image)
       ? item.data.hover_image
-      : fallbackItemImage
+      : fallbackItemImage;
 
     return asImageSrc(image, {
       fit: "crop",
       w: 220,
       h: 320,
       exp: -10,
-    })
-  })
+    });
+  });
 
   useEffect(() => {
     contentImages.forEach((url) => {
-      if (!url) return
-      const img = new Image()
-      img.src = url
-    })
-  }, [contentImages])
+      if (!url) return;
+      const img = new Image();
+      img.src = url;
+    });
+  }, [contentImages]);
 
   const onMouseEnter = (index: number) => {
-    setCurrentItem(index)
-  }
+    setCurrentItem(index);
+  };
 
   const onMouseLeave = () => {
-    setCurrentItem(null)
-  }
+    setCurrentItem(null);
+  };
 
   return (
     <div ref={component}>
       <ul
         className="grid border-b border-b-slate-100"
-        onMouseLeave={onMouseLeave}>
+        onMouseLeave={onMouseLeave}
+      >
         {items.map((item, index) => (
           <>
             {isFilled.keyText(item.data.title) && (
               <li
                 key={index}
                 ref={(el) => {
-                  itemsRef.current[index] = el
+                  itemsRef.current[index] = el;
                 }}
-                className="list-item opacity-0f"
-                onMouseEnter={() => onMouseEnter(index)}>
+                className="list-item opacity-0"
+                onMouseEnter={() => onMouseEnter(index)}
+              >
                 <Link
                   href={urlPrefix + "/" + item.uid}
                   className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-                  aria-label={item.data.title}>
+                  aria-label={item.data.title}
+                >
                   <div className="flex flex-col">
                     <span className="text-3xl font-bold">
                       {item.data.title}
                     </span>
-                    <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                    <div className="flex gap-3 text-lg font-bold text-yellow-400">
                       {item.tags.map((tag, index) => (
                         <span key={index}>{tag}</span>
                       ))}
@@ -159,14 +162,15 @@ function ContentList({
       {/* Hover Element */}
 
       <div
-        className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-over bg-center opacity-0 transition-[background]  duration-300"
+        className="hover-reveal bg-over pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-center opacity-0 transition-[background] duration-300"
         style={{
           backgroundImage:
             currentItem !== null ? `url(${contentImages[currentItem]})` : "",
         }}
-        ref={revealRef}></div>
+        ref={revealRef}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default ContentList
+export default ContentList;
