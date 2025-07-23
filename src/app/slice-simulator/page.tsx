915f178714278b8ca8aaca3f899198bf976d5649
David Hopkins II <dhscience2@gmail.com>
2025-07-23 15:28:49 -0400

    is that better?

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -2,19 +2,20 @@ import {
   SliceSimulator,
   SliceSimulatorParams,
   getSlices,
-} from "@slicemachine/adapter-next/simulator"
-import { SliceZone } from "@prismicio/react"
+} from "@slicemachine/adapter-next/simulator";
+import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices"
+import { components } from "../../slices";
 
-export default function SliceSimulatorPage({
+export default async function SliceSimulatorPage({
   searchParams,
 }: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state)
+  const resolvedParams = await searchParams;
+  const slices = getSlices(resolvedParams?.state);
 
   return (
     <SliceSimulator background="#0f1729">
       <SliceZone slices={slices} components={components} />
     </SliceSimulator>
-  )
+  );
 }
