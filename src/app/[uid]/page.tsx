ec88e7c5e990392b2243395f8cb527b368d80572
David Hopkins II <dhscience2@gmail.com>
2025-08-27 15:52:31 -0400

    Should be better?

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -1,38 +1,44 @@
-import { Metadata } from "next"
-import { notFound } from "next/navigation"
-import { SliceZone } from "@prismicio/react"
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { SliceZone } from "@prismicio/react";
 
-import { createClient } from "@/prismicio"
-import { components } from "@/slices"
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-type Params = { uid: string }
+type Params = { uid: string };
 
-export default async function Page({ params }: { params: Params }) {
-  const client = createClient()
-  const page = await client.getByUID("page", params.uid).catch(() => notFound())
+type PageProps<T> = { params: T };
 
-  return <SliceZone slices={page.data.slices} components={components} />
+export default async function Page({ params }: PageProps<Params>) {
+  const client = createClient();
+  const page = await client
+    .getByUID("page", params.uid)
+    .catch(() => notFound());
+
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata({
   params,
 }: {
-  params: Params
+  params: Params;
 }): Promise<Metadata> {
-  const client = createClient()
-  const page = await client.getByUID("page", params.uid).catch(() => notFound())
+  const client = createClient();
+  const page = await client
+    .getByUID("page", params.uid)
+    .catch(() => notFound());
 
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
-  }
+  };
 }
 
 export async function generateStaticParams() {
-  const client = createClient()
-  const pages = await client.getAllByType("page")
+  const client = createClient();
+  const pages = await client.getAllByType("page");
 
   return pages.map((page) => {
-    return { uid: page.uid }
-  })
+    return { uid: page.uid };
+  });
 }
